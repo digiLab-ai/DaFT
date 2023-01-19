7af9df6422b9b5e1452df65f0beb3070fc89932e
Mikkel Lykkegaard <mikkel@digilab.co.uk>
2023-01-19 12:35:47 +0000

    Renamed P to X and Q to Y, and added docstrings and comments

--- daft/daft.py
+++ daft/daft.py
@@ -2,27 +2,92 @@ from .mmd import RandomMMD
 from .ga import IndexSelectionGA
 
 class DaFT:
-    def __init__(self, P, n_sub, n_features=None, **kwargs):
+    """
+    DaFT is a class that implements the DerivAtive-Free Thinning algorithm.
+
+    Attributes
+    ----------
+    X : numpy.ndarray
+        The reference empirical distribution.
+    n_total : int
+        The number of datapoints in the reference distribution.
+    n_sub : int
+        The required number of datapoints for the thinned distribution.
+    n_features : int
+        The dimension of the feature space. 
+    mmd : daft.mmd.RandomMMD
+        An instance of daft.mmd.RandomMMD.
+    ga : daft.ga.IndexSelectionGA
+        An instance of daft.ga.IndexSelectionGA.
         
-        self.P = P
+    Methods
+    ----------
+    run(**kwargs)
+        Run the DaFT algorithm.
+    """
+
+    def __init__(self, X, n_sub, n_features=None, **kwargs):
+        """
+        Parameters
+        ----------
+        X : numpy.ndarray
+            The reference empirical distribution.
+        n_sub : int
+            The required number of datapoints for the thinned distribution.
+        n_features : int, optional
+            The dimension of the feature space. Default is 1000 x input 
+            dimension.
+        kwargs
+            Keyword arguments passed to daft.ga.IndexSelectionGA.
+        Returns
+        ----------
+        None
+        """
+        
+        # set the reference distribution.
+        self.X = X
+        
+        # set the number of datapoints in the reference distribution.
+        self.n_total = self.X.shape[0]
+        
+        # set the required size of the thinned distribution.
         self.n_sub = n_sub
-        self.n_total = self.P.shape[0]
         
+        # compute the feature space dimension of it's not provided.
         if n_features is None:
-            n_features = P.shape[1]*1000
+            n_features = X.shape[1]*1000
         
+        # set the feature space dimension.
         self.n_features = n_features
         
-        self.mmd = RandomMMD(self.P, self.n_features)
+        # set up an MMD instance.
+        self.mmd = RandomMMD(self.X, self.n_features)
         
+        # set up a GA instance.
         self.ga = IndexSelectionGA(self.n_sub, 
                                    self.n_total, 
                                    self.mmd._get_mmd_ga, 
                                    **kwargs)
         
     def run(self, **kwargs):
+        """
+        Run DaFT.
+        
+        Parameters
+        ----------
+        kwargs
+             Keyword arguments passed to daft.ga.IndexSelectionGA.run().
+            
+        Returns
+        ----------
+        numpy.ndarray
+        """
+        
+        # set the number of chromosomes to return from the GA.
         kwargs["k"] = 1
         
+        # run the GA.
         indices = self.ga.run(**kwargs)[0]
         
-        return self.P[indices,:]
+        # return the thinned distribution.
+        return self.X[indices,:]
--- daft/ga.py
+++ daft/ga.py
@@ -4,39 +4,125 @@ import multiprocessing
 from deap import algorithms, base, creator, tools
 
 class IndexSelectionGA:
-    def __init__(self, n_sub, n_total, fitness_min, **kwargs):
+    """
+    IndexSelectionGA is a class that sets up a Genetic Algorithm that will
+    minimise a fitness function over a list of indices.
+
+    Attributes
+    ----------
+    n_sub : int
+        How many indices to return.
+    n_total : int
+        How many total indices.
+    fitness_min : callable
+        The fitness function to minimise.
+    toolbox : deap.base.Toolbox
+        A DEAP Toolbox instance.
+    indpb_mate : float
+        The probability of swapping genes when mating two chromosomes.
+    indpb_mutate : float
+        The probability of mutating each gene during mutation.
+    tournsize : int
+        The tournament size for chromosome selection.
         
+    Methods
+    ----------
+    run(**kwargs)
+        Run the Genetic Algorithm.
+    """
+
+    def __init__(self, n_sub, n_total, fitness_min, **kwargs):
+        """
+        Parameters
+        ----------
+        n_sub : int
+            How many indices to return.
+        n_total : int
+            How many total indices.
+        fitness_min : callable
+            The fitness function to minimise.
+        multiprocessing : bool, optional.
+            Whether to use multiprocessing, default is True.
+        indpb_mate : float, optional
+            The probability of swapping genes when mating two chromosomes.
+            Default is 0.2
+        indpb_mutate : float, optional
+            The probability of mutating each gene during mutation. Default
+            is 0.1
+        tournsize : int, optional
+            The tournament size for chromosome selection. Default is 3.
+            
+        Returns
+        ----------
+        None
+        """
+
+        # set the number of indices to return and the total number of indices.
         self.n_sub = n_sub
         self.n_total = n_total
         
+        # set the fitness function to be minimised.
         self.fitness_min = fitness_min
         
+        # initialise the fitness function with DEAP.
         creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        
+        # initialise the individial (chromosome) with DEAP.
         creator.create("Individual", list, fitness=creator.FitnessMin)
 
+        # initialise a DEAP Toolbox.
         self.toolbox = base.Toolbox()
-        
+
+        # set up multiprocessing, if requested.
         if kwargs.pop("multiprocessing", True):
             self.toolbox.register("map", multiprocessing.Pool().map)
-        
+
+        # define the parameter space as a random sample from the indices.
         self.toolbox.register("indices", random.sample, range(self.n_total), self.n_sub)
+        
+        # initilise the individuals, population and the evaluation function.
         self.toolbox.register("individual", tools.initIterate, creator.Individual, self.toolbox.indices)
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
         self.toolbox.register("evaluate", self.fitness_min)
         
+        # set up random uniform mating.
         self.indpb_mate = kwargs.pop("indpb_mate", 0.2)
         self.toolbox.register("mate", tools.cxUniform, indpb=self.indpb_mate)
         
+        # set up mutation to be random uniform dram from indices.
         self.indpb_mutate = kwargs.pop("indpb_mutate", 0.1)
         self.toolbox.register("mutate", tools.mutUniformInt, low=0, up=self.n_total-1, indpb=self.indpb_mutate)
         
+        # set up the tournament selection process.
         self.tournsize = kwargs.pop("tournsize", 3)
         self.toolbox.register("select", tools.selTournament, tournsize=self.tournsize)
     
     def run(self, **kwargs):
+        """
+        Run the Genetic Algorithm.
         
+        Parameters
+        ----------
+        n : int, optional.
+            How large a population to use. Default is 100.
+        ngen : int, optional
+            How many generations to run the GA. Default is 100.
+        k : int, optional
+            How many chromosomes to return. Default is 1.
+        cxpb : float, optional
+            The probability of mating. Default is 0.5.
+        mutpb : float, optional
+            The probability of mutating. Default is 0.2.
+            
+        Returns
+        ----------
+        list
+        """
+        
+        # initialise the population.
         population = self.toolbox.population(n=kwargs.pop("n", 100))
         
+        # run the GA.
         algorithms.eaSimple(population, 
                             self.toolbox, 
                             cxpb=kwargs.pop("cxpb", 0.5), 
@@ -44,4 +130,5 @@ class IndexSelectionGA:
                             ngen=kwargs.pop("ngen", 100), 
                             verbose=False)
         
+        # return the top chromosomes.
         return tools.selBest(population, k=kwargs.pop("k", 1))
